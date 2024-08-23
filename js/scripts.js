baf1eee5742dd13b839ad6377f137a1b68ef130b
YouLan203 <lanangel33@gmail.com>
2024-08-23 13:03:35 +0800

    0823update

--- js/scripts.js
+++ js/scripts.js
@@ -5,3 +5,15 @@ function signJump(){
 function login(){
     window.location.href="/home.html";
 }
+
+function go(){
+    window.alert("尚未到抽獎時間！")
+}
+
+function save(){
+    window.alert("保存成功！")
+}
+
+function build(){
+    window.alert("註冊成功！")
+}
