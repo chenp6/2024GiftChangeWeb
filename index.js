2575128a5270991ddf0c5458b0ad48dca0634f14
YouLan203 <lanangel33@gmail.com>
2024-09-02 11:50:02 +0800

    0902update3

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ const port = process.env.PORT || 3000
 
 require('dotenv').config()
 
-server.use('/image', express.static(path.join(process.cwd(), 'image')));
-server.use('/css', express.static(path.join(process.cwd(), 'css')));
-server.use('/js', express.static(path.join(process.cwd(), 'js')));
+server.use('/image', express.static(__dirname + '/image')); //只將某資料夾放到server上
+server.use('/css', express.static(__dirname + '/css'));
+server.use('/js', express.static(__dirname + '/js'));
 
 // 建立 get method 顯示 index.html 內容
 server.get('/', (req, res) => {
